dbfa8e9b1b7185f993dcecd9641408765715765e
Pavel Darii <paveldarii@yahoo.com>
2020-12-29 22:33:16 -0500

    with the help of express.Router() I created routes with methods as GET, POST, DELETE, and PUT

---
+++ routes/api/notes.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const notes = require("../../db/db.json"); // .. outside api folder .. outside routes folder
+const uuid = require("uuid");
+const fs = require("fs");
+const router = express.Router();
+
+// get all members
+router.get("/", (req, res) => res.json(notes));
+
+// get a specific elem
+router.get("/:id", (req, res) => {
+  const found = notes.some((note) => note.id === req.params.id);
+  if (found) {
+    res.json(notes.filter((note) => note.id === req.params.id));
+  } else {
+    res.status(400).json({ msg: `no note with the id of ${req.params.id}` });
+  }
+});
+
+//create a note
+router.post("/", (req, res) => {
+  const newNote = {
+    id: uuid.v4(),
+    title: req.body.title,
+    text: req.body.text,
+    status: "active",
+  };
+  if (!newNote.title || !newNote.text) {
+    return res.status(400).json({ msg: "Please include title or text" });
+  }
+
+  notes.push(newNote);
+
+  let data = JSON.stringify(notes);
+  fs.writeFileSync("db/db.json", data);
+  res.json(notes);
+});
+
+//update note
+
+router.put("/:id", (req, res) => {
+  // check to see if the note exists
+  const found = notes.some((note) => note.id === req.params.id);
+  if (found) {
+    const updNote = req.body;
+    notes.forEach((note) => {
+      if (note.id === req.params.id) {
+        note.title = updNote.title ? updNote.title : note.title;
+        note.text = updNote.text ? updNote.text : note.text;
+        res.json({ msg: "note updated", note }, { notes });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `no note with the id of ${req.params.id}` });
+  }
+  let data = JSON.stringify(notes);
+  fs.writeFileSync("db/db.json", data);
+  res.json(notes);
+});
+
+//Delete note
+
+router.delete("/:id", (req, res) => {
+  const found = notes.some((note) => note.id === req.params.id);
+  let remainedNotes;
+  if (found) {
+    res.json({
+      msg: "Note deleted successfully",
+      notes: notes.filter((note) => note.id !== req.params.id),
+    });
+    remainedNotes = notes.filter((note) => note.id !== req.params.id);
+  } else {
+    res.status(400).json({ msg: `no note with the id of ${req.params.id}` });
+  }
+  remainedNotes = JSON.stringify(remainedNotes);
+  fs.writeFileSync("db/db.json", remainedNotes);
+  res.json(remainedNotes);
+});
+
+module.exports = router;
