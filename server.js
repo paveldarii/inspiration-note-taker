fca599538429e2f219f4b004104b2005aa4a6083
Pavel Darii <paveldarii@yahoo.com>
2021-01-01 17:26:38 -0500

    created get request handler for start and notes pages

--- server.js
+++ server.js
@@ -8,13 +8,18 @@ app.use(logger);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 //to get a static folder
-//app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/notes", require("./routes/api/notes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`server is running on ${PORT}`));
 
