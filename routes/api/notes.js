7f2e7f57b336586b9476b29a927cb8c4fcbfc7cb
Pavel Darii <paveldarii@yahoo.com>
2021-01-05 06:58:29 -0500

    moved the confirmation of deleted message after the note was deleted

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const notes = require("../../db/db.json"); // .. outside api folder .. outside routes folder
+const notes = require("../../db/db.json");
 const uuid = require("uuid");
 const fs = require("fs");
 const router = express.Router();
 
-// get all members
+// get all notes
 router.get("/", (req, res) => res.json(notes));
 
 // get a specific elem
@@ -12,9 +12,8 @@ router.get("/:id", (req, res) => {
   const found = notes.some((note) => note.id === req.params.id);
   if (found) {
     res.json(notes.filter((note) => note.id === req.params.id));
-  } else {
-    res.status(400).json({ msg: `no note with the id of ${req.params.id}` });
   }
+  res.status(400).json({ msg: `no note with the id of ${req.params.id}` });
 });
 
 //create a note
@@ -41,13 +40,13 @@ router.delete("/:id", (req, res) => {
   const found = notes.some((note) => note.id === req.params.id);
   let remainedNotes;
   if (found) {
+    remainedNotes = notes.filter((note) => note.id !== req.params.id);
+    remainedNotes = JSON.stringify(remainedNotes);
+    fs.writeFileSync("db/db.json", remainedNotes);
     res.json({
       msg: "Note deleted successfully",
       notes: notes.filter((note) => note.id !== req.params.id),
     });
-    remainedNotes = notes.filter((note) => note.id !== req.params.id);
-    remainedNotes = JSON.stringify(remainedNotes);
-    fs.writeFileSync("db/db.json", remainedNotes);
   } else {
     res.status(400).json({ msg: `no note with the id of ${req.params.id}` });
   }
