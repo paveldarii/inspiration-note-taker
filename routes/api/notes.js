15c1256f14be57c60722af49da696b4cfd51cb8d
Pavel Darii <paveldarii@yahoo.com>
2021-01-07 13:24:21 -0500

    test

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -43,13 +43,19 @@ router.post("/", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   const found = notes.findIndex((note) => note.id === req.params.id);
+  let remainedNotes;
   if (found >= 0) {
     notes.splice(found, 1);
-    res.json({
-      msg: "Note deleted successfully",
-      notes,
+    fs.writeFile("db/db.json", JSON.stringify(notes), (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.json({
+        msg: "Note deleted successfully",
+        notes: remainedNotesJson,
+      });
     });
-    fs.writeFileSync("db/db.json", JSON.stringify(notes));
   } else {
     res.status(400).json({ msg: `no note with the id of ${req.params.id}` });
   }
